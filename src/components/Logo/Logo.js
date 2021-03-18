e466419d28113c2c24027a1f2df4a4e4a3904234
Aimarall <aimaral21072004@gmail.com>
2021-03-18 14:14:46 +0600

    removed the empty pfigure bracket

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import classes from "./Logo.module.css";
 import logo from "../../images/logo.svg";
 
 
-const Logo = ({ }) => {
+const Logo = () => {
     return (
         <div className={classes.Logo}>
             <img src={logo} />
