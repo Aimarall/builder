921fc6a14a2682c42d58d186a8ba196fd703df31
Aimarall <aimaral21072004@gmail.com>
2021-03-19 16:34:38 +0600

    added module.css

---
+++ src/components/Layout/CeilingControls/CeilingControls.js
@@ -0,0 +1,11 @@
+import classes from "./CeilingControls.module.css";
+
+
+const CeilingControls = () => {
+  return (
+    <div className={classes.CeilingControls}></div>
+    
+  );
+}
+
+export default CeilingControls;
