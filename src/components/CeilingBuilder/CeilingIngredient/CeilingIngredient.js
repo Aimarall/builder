920028c3486232c71ec75f38162b9946114c3693
Aimarall <aimaral21072004@gmail.com>
2021-03-22 22:32:00 +0600

    added component  CeilingIngredient

---
+++ src/components/CeilingBuilder/CeilingIngredient/CeilingIngredient.js
@@ -0,0 +1,9 @@
+import classes from "./CeilingIngredient.module.css";
+
+const CeilingIngredient = ({ type }) => {
+  return (
+    <div className={classes.CeilingIngredient}>{type}</div>
+  );
+}
+
+export default CeilingIngredient;
