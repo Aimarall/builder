92f2a11a31c46291facb455941aab7f0f55c5e40
Aimarall <aimaral21072004@gmail.com>
2021-04-08 19:57:15 +0600

    added const to fixing files

---
+++ src/components/FixingBuilder/FixingBuilder.js
@@ -0,0 +1,252 @@
+// import CeilingPreview from "./CeilingPreview/CeilingPreview";
+// import CeilingControls from "./CeilingPreview/CeilingPreview";
+
+// import classes from "./CeilingBuilder.module.css";
+// import { useState } from "react";
+// import axios from "axios";
+// import Modal from "../UI/Modal/Modal";
+// import OrderSummary from "./OrderSummary/OrderSummary";
+
+// const CeilingBuilder = () => {
+//   const prices = {
+//     tomato: 5,
+//     salami: 4,
+//     blackOlive: .5,
+//     greenOlive: .5,
+//     redPepper: 3,
+//     yellowPepper: 2,
+  
+//   };
+ 
+//   const [ingredients, setIngredients] = useState({});
+//   const [price, setPrice] = useState(0);
+//   const [ordering, setOrdering] = useState(false);
+
+//   useEffect(
+//     () => axios
+//       .get('https://builder2-97915-default-rtdb.firebaseio.com/default.json')
+//       .then(response => {
+//         setPrice(response.data.price);
+
+//         // For arrays
+//         // setIngredients(Object.values(response.data.ingredients));
+//         // For objects
+//         setIngredients(response.data.ingredients);
+//       }), []
+//   );
+
+//   function addIngredient(type) {
+//     const newIngredients = { ...ingredients };
+//     newIngredients[type]++;
+
+//     checkCanBuy(newIngredients);
+
+//     setPrice(price + prices[type]);
+//     setIngredients(newIngredients);
+//   }
+
+//   function removeIngredient(type) {
+//     if (ingredients[type]) {
+//       const newIngredients = { ...ingredients };
+//       newIngredients[type]--;
+//       setPrice(price - prices[type]);
+//       setIngredients(newIngredients);
+//     }
+//   }
+  
+//   function startOrdering() {
+//     setOrdering(true);
+//   }
+
+//   function stopOrdering() {
+//     setOrdering(false);
+//   }
+
+//   return (
+//     <div className={classes.CeilingBuilder}>
+//       <CeilingPreview
+//         ingredients={ingredients}
+//         price={price} />
+//       <CeilingControls
+//         ingredients={ingredients}
+//         addIngredient={addIngredient}
+//         removeIngredient={removeIngredient}
+//         startOrdering={startOrdering}
+//         />
+//       <Modal
+//         show={ordering}
+//         cancel={stopOrdering}>Hello</Modal>
+//     </div>
+//   );
+// }
+
+// export default CeilingBuilder;
+
+
+
+
+
+
+
+
+
+
+
+
+// import CeilingPreview from "./CeilingPreview/CeilingPreview";
+// import CeilingControls from "./CeilingControls/CeilingControls";
+
+// import classes from "./CeilingBuilder.module.css";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import Modal from "../UI/Modal/Modal";
+
+// const CeilingBuilder = () => {
+//   const prices = {
+//     tomato: 3.5,
+//     salami: 4,
+//     greenOlive: .3,
+//     blackOlive: .3,
+//     redPepper: 2,
+//     yellowPepper: 1,
+//   };
+//   const [ingredients, setIngredients] = useState({});
+//   const [price, setPrice] = useState(0);
+//   const [ordering, setOrdering] = useState(false);
+
+//   useEffect(
+//     () => axios
+//       .get('https://builder2-97915-default-rtdb.firebaseio.com/default.json')
+//       .then(response => {
+//         setPrice(response.data.price);
+
+//         // For arrays
+//         // setIngredients(Object.values(response.data.ingredients));
+//         // For objects
+//         setIngredients(response.data.ingredients);
+//       }), []
+//   );
+
+//   function addIngredient(type) {
+//     const newIngredients = { ...ingredients };
+//     newIngredients[type]++;
+//     setPrice(price + prices[type]);
+//     setIngredients(newIngredients);
+//   }
+
+//   function removeIngredient(type) {
+//     if (ingredients[type]) {
+//       const newIngredients = { ...ingredients };
+//       newIngredients[type]--;
+//       setPrice(price - prices[type]);
+//       setIngredients(newIngredients);
+//     }
+//   }
+
+//   function startOrdering() {
+//     setOrdering(true);
+//   }
+
+//   function stopOrdering() {
+//     setOrdering(false);
+//   }
+
+//   return (
+//     <div className={classes.CeilingBuilder}>
+//       <CeilingPreview
+//         ingredients={ingredients}
+//         price={price} />
+//       <CeilingControls
+//         ingredients={ingredients}
+//         addIngredient={addIngredient}
+//         removeIngredient={removeIngredient}
+//         startOrdering={startOrdering}
+//         />
+//       <Modal
+//         show={ordering}
+//         cancel={stopOrdering}>Hello</Modal>
+//     </div>
+//   );
+// }
+
+// export default CeilingBuilder;
+
+
+
+import FixingPreview from "./FixingPreview/FixingPreview";
+import FixingControls from "./FixingControls/FixingControls";
+
+import classes from "./FixingBuilder.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Modal from "../UI/Modal/Modal";
+
+const FixingBuilder = () => {
+  const prices = {
+    tomato: 3.5,
+    salami: 4,
+    greenOlive: .3,
+    blackOlive: .3,
+    redPepper: 2,
+    yellowPepper: 1,
+  };
+  const [ingredients, setIngredients] = useState({});
+  const [price, setPrice] = useState(0);
+  const [ordering, setOrdering] = useState(false);
+
+  useEffect(
+    () => axios
+      .get('https://builder2-97915-default-rtdb.firebaseio.com/default.json')
+      .then(response => {
+        setPrice(response.data.price);
+
+        // For arrays
+        // setIngredients(Object.values(response.data.ingredients));
+        // For objects
+        setIngredients(response.data.ingredients);
+      }), []
+  );
+
+  function addIngredient(type) {
+    const newIngredients = { ...ingredients };
+    newIngredients[type]++;
+    setPrice(price + prices[type]);
+    setIngredients(newIngredients);
+  }
+
+  function removeIngredient(type) {
+    if (ingredients[type]) {
+      const newIngredients = { ...ingredients };
+      newIngredients[type]--;
+      setPrice(price - prices[type]);
+      setIngredients(newIngredients);
+    }
+  }
+
+  function startOrdering() {
+    setOrdering(true);
+  }
+
+  function stopOrdering() {
+    setOrdering(false);
+  }
+
+  return (
+    <div className={classes.FixingBuilder}>
+      <FixingPreview
+        ingredients={ingredients}
+        price={price} />
+      <FixingControls
+        ingredients={ingredients}
+        addIngredient={addIngredient}
+        removeIngredient={removeIngredient}
+        startOrdering={startOrdering}
+        />
+      <Modal
+        show={ordering}
+        cancel={stopOrdering}>Hello</Modal>
+    </div>
+  );
+}
+
+export default FixingBuilder;
