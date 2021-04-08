2a0e3d38099419225b7164b2a79f41b4f2359df3
Aimarall <aimaral21072004@gmail.com>
2021-04-08 19:53:04 +0600

    deleted one lements of the this file

--- src/components/UI/Logo/Logo.js
+++ src/components/UI/Logo/Logo.js
@@ -1,11 +1,28 @@
+
+
+// import classes from "./Logo.module.css";
+// import logo from "../../../images/logo.svg";
+
+// const Logo = () => {
+//   return (
+//     <div className={classes.Logo}>
+//       <img src={logo} alt="Builder" />
+//       <div>Ceiling</div>
+//     </div>
+//   );
+// }
+
+// export default Logo;
+// //<img src={logo} alt="Logo of the Ceiling project" />
+
 import classes from "./Logo.module.css";
 import logo from "../../../images/logo.svg";
 
 const Logo = () => {
   return (
     <div className={classes.Logo}>
-      <img src={logo} alt="Fixing logo" />
-      <div>Fixing Builder</div>
+      <img src={logo} alt="Logo" />
+      <div>Fixing</div>
     </div>
   );
 }
