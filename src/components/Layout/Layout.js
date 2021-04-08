3981494a986ca2a675329a48e2d8ac63c76cf64e
Aimarall <aimaral21072004@gmail.com>
2021-04-08 20:04:02 +0600

    deleted not needed function of the files

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,38 @@
-import classes from "./Layout.module.css";
+// import { useState } from "react";
+// import Toolbar from "./Toolbar/Toolbar";
+// import Drawer from "./Drawer/Drawer";
+
+// import classes from "./Layout.module.css";
+
+// const Layout = ({ children }) => {
+//   const [drawerOpen, setDrawerOpen] = useState(false);
+
+//   return (
+//     <div className={classes.Layout}>
+//       <Toolbar openDrawer={() => setDrawerOpen(true)} />
+//       <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
+//       <main>
+//         {children}
+//       </main>
+//     </div>
+//   );
+// }
+
+// export default Layout;
+
+import { useState } from "react";
 import Toolbar from "./Toolbar/Toolbar";
 import Drawer from "./Drawer/Drawer";
-import { useState } from "react";
+
+import classes from "./Layout.module.css";
 
 const Layout = ({ children }) => {
   const [drawerOpen, setDrawerOpen] = useState(false);
 
   return (
     <div className={classes.Layout}>
-     
       <Toolbar openDrawer={() => setDrawerOpen(true)} />
       <Drawer open={drawerOpen} closeDrawer={() => setDrawerOpen(false)} />
-
       <main>
         {children}
       </main>
