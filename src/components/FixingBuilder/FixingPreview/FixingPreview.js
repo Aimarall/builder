2483f4a2f4be30ab2977860e8ff03e24c8ca1327
Aimarall <aimaral21072004@gmail.com>
2021-04-09 13:27:49 +0600

    changed color of the text

--- src/components/FixingBuilder/FixingPreview/FixingPreview.js
+++ src/components/FixingBuilder/FixingPreview/FixingPreview.js
@@ -1,22 +1,22 @@
-
-// import PizzaIngredient from "../PizzaIngredient/PizzaIngredient";
-
+// import FixingIngredient from "../FixingIngredient/FixingIngredient";
 // import classes from "./PizzaPreview.module.css";
 // import ingredientsBackground from "../../../images/cheese.svg";
 
-// const PizzaPreview = ({ ingredients, price }) => {
+
+// const FixingPreview = ({ ingredients, price }) => {
 //   const result = [];
 //   for (const ingredient in ingredients) {
 //     const example = [];
 //     for (let i = 0; i < ingredients[ingredient]; i++) {
-//       example.push(<PizzaIngredient key={ingredient + i} type={ingredient} />)
+//       example.push(<FixingIngredient key={ingredient + i} type={ingredient} />)
 //     }
 //     result.push(<div>{example}</div>)
 //   }
 
+  
 //   return (
-//     <div className={classes.PizzaPreview}>
-//       <div className={classes.pizza}>
+//     <div className={classes.FixingPreview}>
+//       <div className={classes.fixing}>
 //         <div
 //           className={classes.ingredients}
 //           style={{ backgroundImage: `url(${ingredientsBackground})` }}>
@@ -28,7 +28,7 @@
 //   );
 // }
 
-// export default PizzaPreview;
+// export default FixingPreview;
 
 
 
@@ -40,46 +40,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// import CeilingIngredient from "../CeilingIngredient/CeilingIngredient";
-
-// import classes from "./CeilingPreview.module.css";
-// import ingredientsBackground from "../../../images/cheese.svg";
-
-// const CeilingPreview = ({ ingredients, price }) => {
-//   const result = [];
-//   for (const ingredient in ingredients) {
-//     for (let i = 0; i < ingredients[ingredient]; i++) {
-//       result.push(<CeilingIngredient key={ingredient + i} type={ingredient} />)
-//     }
-//   }
-
-//   return (
-//     <div className={classes.CeilingPreview}>
-//       <div className={classes.pizza}>
-//         <div
-//           className={classes.ingredients}
-//           style={{ backgroundImage: `url(${ingredientsBackground})` }}>
-//           {result}
-//         </div>
-//       </div>
-//       <div className={classes.price}>{price.toFixed(1)} som</div>
-//     </div>
-//   );
-// }
-
-// export default CeilingPreview;
 
 import FixingIngredient from "../FixingIngredient/FixingIngredient";
 
