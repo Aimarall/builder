e6a19caf23ed0c3ef7f7f9c789872433fb97de03
Aimarall <aimaral21072004@gmail.com>
2021-04-11 01:02:17 +0600

    chnaged of the preview

--- src/components/FixingBuilder/FixingIngredient/FixingIngredient.js
+++ src/components/FixingBuilder/FixingIngredient/FixingIngredient.js
@@ -68,73 +68,73 @@
 
 
 
-// import React from "react";
-// import classes from "./FixingIngredient.module.css";
-
-// import izmurud1Background from "../../../images/izmurud1.png";
-// import zoloto2Background from "../../../images/zoloto2.svg";
-// import silver2Background from "../../../images/silver2.svg";
-// import bronze1Background from "../../../images/bronze1.svg";
-// import almaz1Background from "../../../images/almaz1.svg";
-// import rubin1Background from "../../../images/rubin1.svg";
-// import amber1Background from "../../../images/amber1.svg";
 
 
+import React from "react";
+import classes from "./FixingIngredient.module.css";
 
-// const FixingIngredient = ({ type, fixed }) => {
-//   const types = {
-//     izmurud1: { backgroundImage: `url(${izmurud1Background})`, width: "50px", height: "50px" },
-//     zoloto2: { backgroundImage: `url(${zoloto2Background})`, width: "55px", height: "55px" },
-//     silver2: { backgroundImage: `url(${silver2Background})`, width: "64px", height: "64px" },
-//     bronze1: { backgroundImage: `url(${bronze1Background})`, width: "64px", height: "64px" },
-//     almaz1: { backgroundImage: `url(${almaz1Background})`, width: "50px", height: "50px" },
-//     rubin1: { backgroundImage: `url(${rubin1Background})`, width: "50px", height: "50px" },
-//     amber1: { backgroundImage: `url(${amber1Background})`, width: "50px", height: "50px" },
-
-
-//   };
-
-//   function getPosition(ingredientWidth) {
-//     const pizzaDiameter = 370;
-//     const pizzaRadius = pizzaDiameter / 3;
-//     const ingredientRadius = parseInt(ingredientWidth) / 3;
-
-//     const ingredientTop = Math.round(Math.random() * pizzaDiameter);
-//     const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
-//     const ingredientRight = Math.round(Math.random() * pizzaDiameter);
+import izmurud1Background from "../../../images/izmurud1.png";
+import zoloto2Background from "../../../images/zoloto2.svg";
+import silver2Background from "../../../images/silver2.svg";
+import almaz1Background from "../../../images/almaz1.svg";
+import rubin1Background from "../../../images/rubin1.svg";
+import amber1Background from "../../../images/amber1.svg";
 
 
-//     const distance = Math.sqrt(
-//       Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 1) 
-//       + Math.pow(ingredientRight - pizzaRadius, 2)
-//     ) + ingredientRadius;
 
-//     return distance < pizzaRadius
-//       ? {
-//         top: ingredientTop - ingredientRadius,
-//         left: ingredientLeft - ingredientRadius,
-//         left: ingredientRight- ingredientRadius, //-
+const FixingIngredient = ({ type, fixed }) => {
+  const types = {
+    izmurud1: { backgroundImage: `url(${izmurud1Background})`, width: "50px", height: "50px" },
+    zoloto2: { backgroundImage: `url(${zoloto2Background})`, width: "55px", height: "55px" },
+    silver2: { backgroundImage: `url(${silver2Background})`, width: "64px", height: "64px" },
+    almaz1: { backgroundImage: `url(${almaz1Background})`, width: "50px", height: "50px" },
+    rubin1: { backgroundImage: `url(${rubin1Background})`, width: "50px", height: "50px" },
+    amber1: { backgroundImage: `url(${amber1Background})`, width: "50px", height: "50px" },
 
-//       }
-//       : getPosition(ingredientWidth);
-//   }
 
-//   // Get random position for this ingredient.
-//   if (!fixed) {
-//     const position = getPosition(types[type].width);
-//     types[type].top = position.top + "px";
-//     types[type].left = position.left + "px";
-//     types[type].right = position.right + "px";
-//   }
-//   // Get random rotation for this ingredient.
-//   types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+  };
 
-//   return (
-//     <div className={classes.FixingIngredient} style={types[type]}></div>
-//   );
-// }
+  function getPosition(ingredientWidth) {
+    const pizzaDiameter = 360;
+    const pizzaRadius = pizzaDiameter / 5;
+    const ingredientRadius = parseInt(ingredientWidth) / 7;
 
-// export default React.memo(FixingIngredient);
+    const ingredientTop = Math.round(Math.random() * pizzaDiameter);
+    const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
+    const ingredientRight = Math.round(Math.random() * pizzaDiameter);
+
+
+    const distance = Math.sqrt(
+      Math.pow(ingredientTop - pizzaRadius, 3) + Math.pow(ingredientLeft - pizzaRadius, 1) 
+      + Math.pow(ingredientRight - pizzaRadius, 3)
+    ) + ingredientRadius;
+
+    return distance < pizzaRadius
+      ? {
+        top: ingredientTop - ingredientRadius,
+        left: ingredientLeft - ingredientRadius,
+        left: ingredientRight- ingredientRadius, //-
+
+      }
+      : getPosition(ingredientWidth);
+  }
+
+  // Get random position for this ingredient.
+  if (!fixed) {
+    const position = getPosition(types[type].width);
+    types[type].top = position.top + "px";
+    types[type].left = position.left + "px";
+    types[type].right = position.right + "px";
+  }
+  // Get random rotation for this ingredient.
+  types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+
+  return (
+    <div className={classes.FixingIngredient} style={types[type]}></div>
+  );
+}
+
+export default React.memo(FixingIngredient);
 
 
 
@@ -288,72 +288,72 @@
 
 
 
-import React from "react";
-import classes from "./FixingIngredient.module.css";
+// import React from "react";
+// import classes from "./FixingIngredient.module.css";
 
-import izmurud1Background from "../../../images/izmurud1.png";
-import zoloto2Background from "../../../images/zoloto2.svg";
-import silver2Background from "../../../images/silver2.svg";
-import bronze1Background from "../../../images/bronze1.svg";
-import almaz1Background from "../../../images/almaz1.svg";
-import rubin1Background from "../../../images/rubin1.svg";
-import amber1Background from "../../../images/amber1.svg";
-
-
-
-const FixingIngredient = ({ type, fixed }) => {
-  const types = {
-    izmurud1: { backgroundImage: `url(${izmurud1Background})`, width: "50px", height: "50px" },
-    zoloto2: { backgroundImage: `url(${zoloto2Background})`, width: "55px", height: "55px" },
-    silver2: { backgroundImage: `url(${silver2Background})`, width: "64px", height: "64px" },
-    bronze1: { backgroundImage: `url(${bronze1Background})`, width: "64px", height: "64px" },
-    almaz1: { backgroundImage: `url(${almaz1Background})`, width: "50px", height: "50px" },
-    rubin1: { backgroundImage: `url(${rubin1Background})`, width: "50px", height: "50px" },
-    amber1: { backgroundImage: `url(${amber1Background})`, width: "50px", height: "50px" },
+// import izmurud1Background from "../../../images/izmurud1.png";
+// import zoloto2Background from "../../../images/zoloto2.svg";
+// import silver2Background from "../../../images/silver2.svg";
+// import bronze1Background from "../../../images/bronze1.svg";
+// import almaz1Background from "../../../images/almaz1.svg";
+// import rubin1Background from "../../../images/rubin1.svg";
+// import amber1Background from "../../../images/amber1.svg";
 
 
-  };
 
-  function getPosition(ingredientWidth) {
-    const pizzaDiameter = 370;
-    const pizzaRadius = pizzaDiameter / 2;
-    const ingredientRadius = parseInt(ingredientWidth) / 2;
+// const FixingIngredient = ({ type, fixed }) => {
+//   const types = {
+//     izmurud1: { backgroundImage: `url(${izmurud1Background})`, width: "50px", height: "50px" },
+//     zoloto2: { backgroundImage: `url(${zoloto2Background})`, width: "55px", height: "55px" },
+//     silver2: { backgroundImage: `url(${silver2Background})`, width: "64px", height: "64px" },
+//     bronze1: { backgroundImage: `url(${bronze1Background})`, width: "64px", height: "64px" },
+//     almaz1: { backgroundImage: `url(${almaz1Background})`, width: "50px", height: "50px" },
+//     rubin1: { backgroundImage: `url(${rubin1Background})`, width: "50px", height: "50px" },
+//     amber1: { backgroundImage: `url(${amber1Background})`, width: "50px", height: "50px" },
 
-    const ingredientTop = Math.round(Math.random() * pizzaDiameter);
-    const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
-    const ingredientRight = Math.round(Math.random() * pizzaDiameter);
+
+//   };
+
+//   function getPosition(ingredientWidth) {
+//     const pizzaDiameter = 370;
+//     const pizzaRadius = pizzaDiameter / 2;
+//     const ingredientRadius = parseInt(ingredientWidth) / 2;
+
+//     const ingredientTop = Math.round(Math.random() * pizzaDiameter);
+//     const ingredientLeft = Math.round(Math.random() * pizzaDiameter);
+//     const ingredientRight = Math.round(Math.random() * pizzaDiameter);
 
 
-    const distance = Math.sqrt(
-      Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 1) 
-      + Math.pow(ingredientRight - pizzaRadius, 2)
-    ) + ingredientRadius;
+//     const distance = Math.sqrt(
+//       Math.pow(ingredientTop - pizzaRadius, 2) + Math.pow(ingredientLeft - pizzaRadius, 1) 
+//       + Math.pow(ingredientRight - pizzaRadius, 2)
+//     ) + ingredientRadius;
 
-    return distance < pizzaRadius
-      ? {
-        top: ingredientTop - ingredientRadius,
-        left: ingredientLeft - ingredientRadius,
-        left: ingredientRight- ingredientRadius, //-
+//     return distance < pizzaRadius
+//       ? {
+//         top: ingredientTop - ingredientRadius,
+//         left: ingredientLeft - ingredientRadius,
+//         left: ingredientRight- ingredientRadius, //-
 
-      }
-      : getPosition(ingredientWidth);
-  }
+//       }
+//       : getPosition(ingredientWidth);
+//   }
 
-  // Get random position for this ingredient.
-  if (!fixed) {
-    const position = getPosition(types[type].width);
-    types[type].top = position.top + "px";
-    types[type].left = position.left + "px";
-    types[type].right = position.right + "px";
-  }
-  // Get random rotation for this ingredient.
-  types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
+//   // Get random position for this ingredient.
+//   if (!fixed) {
+//     const position = getPosition(types[type].width);
+//     types[type].top = position.top + "px";
+//     types[type].left = position.left + "px";
+//     types[type].right = position.right + "px";
+//   }
+//   // Get random rotation for this ingredient.
+//   types[type].transform = `rotate(${Math.round(Math.random() * 360)}deg)`;
 
-  return (
-    <div className={classes.FixingIngredient} style={types[type]}></div>
-  );
-}
+//   return (
+//     <div className={classes.FixingIngredient} style={types[type]}></div>
+//   );
+// }
 
-export default React.memo(FixingIngredient);
+// export default React.memo(FixingIngredient);
 
 
