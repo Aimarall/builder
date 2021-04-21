6ff471a99ce1634b8604f1b72c6f4ff711ba2110
Aimarall <aimaral21072004@gmail.com>
2021-04-21 11:57:57 +0600

    changed  text background-color radius and other

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,12 +1,3 @@
-// import classes from "./Button.module.css";
-
-// const Button = (props) => {
-//   return (
-//     <button {...props} className={classes.Button}>{props.children}</button>
-//   );
-// }
-
-// export default Button;
 
 import classes from "./Button.module.css";
 
