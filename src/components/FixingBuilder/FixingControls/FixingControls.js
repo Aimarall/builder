e5ad31af9ce7a9d9ca9c2e4fe27c4b3e80f85d26
Aimarall <aimaral21072004@gmail.com>
2021-04-21 11:56:39 +0600

    changed  text background-color radius and other

--- src/components/FixingBuilder/FixingControls/FixingControls.js
+++ src/components/FixingBuilder/FixingControls/FixingControls.js
@@ -1,40 +1,4 @@
 
-
-// import Button from "../../UI/Button/Button";
-// import PizzaControl from "./CeilingControl/CeilingControl";
-// import classes from "./CeilingControls.module.css";
-
-// const CeilingControls = ({
-//   ingredients,
-//   addIngredient,
-//   removeIngredient,
-//   startOrdering
-// }) => {
-//   const results = [];
-//   let total = 0;
-//   for (const ingredient in ingredients) {
-//     // Add ingredient number to totals number
-//     total += ingredients[ingredient];
-//     // Render pizza control for this ingredient
-//     results.push(<PizzaControl
-//         key={ingredient}
-//         add={addIngredient}
-//         remove={removeIngredient}
-//         count={ingredients[ingredient]}
-//         type={ingredient} />)
-//   }
-
-//   return (
-//     <div className={classes.CeilingControls}>
-//       <strong>Ingredients</strong>
-//       {results}
-//       <Button disabled={!total} onClick={startOrdering}>Order</Button>
-//     </div>
-//   );
-// }
-
-// export default CeilingControls;
-
 import Button from "../../UI/Button/Button";
 import FixingControl from "./FixingControl/FixingControl";
 import classes from "./FixingControls.module.css";
