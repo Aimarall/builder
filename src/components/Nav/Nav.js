19f84f3296fb26de9c43b1ae41b0c045a204875c
Aimarall <aimaral21072004@gmail.com>
2021-04-21 11:57:38 +0600

    changed  text background-color radius and other

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,3 @@
-// import classes from "./Nav.module.css";
-// import NavItem from "./NavItem/NavItem";
-
-
-// const Nav = () => {
-//   return (
-//     <ul className={classes.Nav}>
-//       <NavItem url="/" active>Builder</NavItem>
-//       <NavItem url="/">Checkout</NavItem>
-     
-//     </ul>
-//   );
-// }
-
-// export default Nav;
-
 
 import classes from "./Nav.module.css";
 import NavItem from "./NavItem/NavItem";
