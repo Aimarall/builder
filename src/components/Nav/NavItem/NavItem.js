53af1d7d71dbba9705f71af694e9b603aa30808e
Aimarall <aimaral21072004@gmail.com>
2021-04-21 11:57:48 +0600

    changed  text background-color radius and other

--- src/components/Nav/NavItem/NavItem.js
+++ src/components/Nav/NavItem/NavItem.js
@@ -1,19 +1,4 @@
 
-// import { NavLink } from "react-router-dom";
-// import classes from "./NavItem.module.css";
-
-// const NavItem = ({ children, url, active }) => {
-//   return (
-//     <li className={classes.NavItem}>
-
-//       <NavLink to={url} activeClassName={classes.active} exact={exact}>{children}</NavLink>
-
-//     </li>
-//   );
-// }
-
-// export default NavItem;
-
 import { NavLink } from "react-router-dom";
 import classes from "./NavItem.module.css";
 
