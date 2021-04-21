3fc904fc0d2bfc4b38c510ec1e545a6a0334117c
Aimarall <aimaral21072004@gmail.com>
2021-04-21 11:57:22 +0600

    changed  text background-color radius and other

--- src/components/Layout/Drawer/DrawerOpen/DrawerOpen.js
+++ src/components/Layout/Drawer/DrawerOpen/DrawerOpen.js
@@ -1,19 +1,4 @@
 
-
-// import classes from "./DrawerOpen.module.css";
-
-// const DrawerOpen = ({ click }) => {
-//   return (
-//     <div className={classes.DrawerOpen} onClick={click}>
-//       <div></div>
-//       <div></div>
-//       <div></div>
-//     </div>
-//   );
-// }
-
-// export default DrawerOpen;
-
 import classes from "./DrawerOpen.module.css";
 
 const DrawerOpen = ({ click }) => {
