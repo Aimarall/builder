8b03dbc19e58101e7c4c0e8008b4af64210c1b75
Aimarall <aimaral21072004@gmail.com>
2021-04-23 15:53:31 +0600

    changed text of logo

--- src/components/FixingBuilder/FixingControls/FixingControl/FixingControl.js
+++ src/components/FixingBuilder/FixingControls/FixingControl/FixingControl.js
@@ -4,6 +4,8 @@ import FixingIngredient from "../../FixingIngredient/FixingIngredient";
 import classes from "./FixingControl.module.css";
 
 const FixingControl = ({ type, add, remove, count }) => {
+  
+
   return (
     <div className={classes.FixingControl}>
       <Button onClick={() => add(type)}>+</Button>
@@ -16,3 +18,42 @@ const FixingControl = ({ type, add, remove, count }) => {
 }
 
 export default FixingControl;
+
+
+
+
+
+
+
+
+
+
+
+
+// import classes from "./SandwichControl.module.css";
+// import Button from "../../../UI/Button/Button";
+
+
+// const SandwichControl = ({ type, add, remove }) => {
+  // const name ={
+  //   cheese:"cheese",
+  //   tomato:" tomato",
+  //   salad:"salad",
+  //   meat:"meat",
+  //   Bacon:"Bacon",
+  //   onions:"onions",
+  // }
+//   return (
+//     <div className={classes.SandwichControl}>
+//  <Button onClick={() => add(type)}>+</Button>
+//       <div className={classes.ingredient}>
+//         {/* <ToysIngredient type={type} fixed /> */}
+//         {name[type]}
+//       </div>
+//       <Button onClick={() => remove(type)}>-</Button>
+//     </div>
+//   );
+// }
+
+
+// export default SandwichControl;
