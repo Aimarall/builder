a81dfb7d079e3e182d91f65a4f949201ad36f64d
Aimarall <aimaral21072004@gmail.com>
2021-04-29 15:28:48 +0600

    changed color text functions and others

--- src/components/Nav/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/components/Nav/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -1,25 +1,51 @@
+// import classes from "./CheckoutSummary.module.css";
+// import FixingPrevew from "../../FixingBuilder/FixingPrevew/FixingPrevew";
+// import Button from "../../UI/Button/Button";
+
+// const CheckoutSummary = ({ cancelCallback }) => {
+//   return (
+//     <div className={classes.CheckoutSummary}>
+//       <div>
+//         <FixingPrevew ingredients={{
+//           almaz1: 3,
+//           amber1: 2,
+//           rauchtopaz: 3,
+//           rubin1: 2,
+//           silver1: 2,
+//           zoloto2: 2,
+//         }} price={150} />
+//       </div>
+//       <div>
+//         <Button>Checkout</Button>
+//         <Button onClick={cancelCallback}>Cancel</Button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default CheckoutSummary;
 
 import classes from "./CheckoutSummary.module.css";
-import FixingPrevew from "../../FixingBuilder/FixingPrevew/FixingPrevew";
+import FixingPreview from "../../FixingBuilder/FixingPreview/FixingPreview";
 import Button from "../../UI/Button/Button";
+import CheckoutForm from "./CheckoutForm/CheckoutForm";
 
-const CheckoutSummary = ({ cancelCallback }) => {
+const CheckoutSummary = ({ submitCallback, cancelCallback }) => {
   return (
     <div className={classes.CheckoutSummary}>
       <div>
-        <FixingPrevew ingredients={{
-          almaz1: 180,
-          amber1: 140,
-          izmurud1: 80,
-          rubin1: 130,
-          silver2: 60,
-          zoloto2: 90,
+        <FixingPreview ingredients={{
+          almaz1: 3,
+          amber1: 2,
+          rauchtopaz1: 3,
+          rubin1: 2,
+          silver1: 2,
+          zoloto2: 2,
         }} price={150} />
       </div>
-      <div>
-        <Button>Checkout</Button>
-        <Button onClick={cancelCallback}>Cancel</Button>
-      </div>
+      <CheckoutForm
+        submitCallback={submitCallback}
+        cancelCallback={cancelCallback} />
     </div>
   );
 }
