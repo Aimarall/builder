40f4f055aa83ed3bca7f0749f04f44a2dcf7464a
Aimarall <aimaral21072004@gmail.com>
2021-04-29 15:28:02 +0600

    deleted noneeded functions

--- src/components/FixingBuilder/FixingControls/FixingControl/FixingControl.js
+++ src/components/FixingBuilder/FixingControls/FixingControl/FixingControl.js
@@ -1,55 +1,27 @@
-
-// import Button from "../../../UI/Button/Button";
-// import FixingIngredient from "../../FixingIngredient/FixingIngredient";
-// import classes from "./FixingControl.module.css";
-
-// const FixingControl = ({ type, add, remove, count }) => {
-  
-
-//   return (
-//     <div className={classes.FixingControl}>
-//       <Button onClick={() => add(type)}>+</Button>
-//       <div className={classes.ingredient}>
-//         <FixingIngredient type={type} fixed />
-//       </div>
-//       <Button onClick={() => remove(type)} disabled={!count}>-</Button>
-//     </div>
-//   );
-// }
-
-// export default FixingControl;
-
-
-
-
-
-
-
-
 import classes from "./FixingControl.module.css";
 import Button from "../../../UI/Button/Button";
 
 
-const FixingControl = ({ type, add, remove }) => {
+const FixingControl = ({ type, add, remove, count}) => {
   const name = {
-    almaz1:"almaz1",
-    amber1:" amber1",
-    rubin1:"rubin1",
-    silver1:"silver2",
-    zoloto2:"zoloto2",
-    rauchtopaz1:"rauchtopaz1",
+    almaz1: "almaz1",
+    amber1: " amber1",
+    rubin1: "rubin1",
+    silver1: "silver2",
+    zoloto2: "zoloto2",
+    rauchtopaz1: "rauchtopaz1",
   }
   return (
     <div className={classes.FixingControl}>
- <Button onClick={() => add(type)}>+</Button>
+      <Button onClick={() => add(type)}>+</Button>
       <div className={classes.ingredient}>
         {/* <ToysIngredient type={type} fixed /> */}
         {name[type]}
       </div>
-      <Button onClick={() => remove(type)}>-</Button>
+      <Button onClick={() => remove(type)}  disabled={!count}>-</Button>
     </div>
   );
 }
 
-  
+
 export default FixingControl;
