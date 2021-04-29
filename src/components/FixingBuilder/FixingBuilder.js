9f0761a2ab048a019e7434f4a40abf5d9811eeb0
Aimarall <aimaral21072004@gmail.com>
2021-04-29 15:27:22 +0600

    added function

--- src/components/FixingBuilder/FixingBuilder.js
+++ src/components/FixingBuilder/FixingBuilder.js
@@ -1,16 +1,102 @@
 
+// import FixingPreview from "./FixingPreview/FixingPreview";
+// import FixingControls from "./FixingControls/FixingControls";
+// import classes from "./FixingBuilder.module.css";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
+// import Modal from "../UI/Modal/Modal";
+
+// const FixingBuilder = () => {
+//   const prices = {
+    // almaz1: 3,
+    // amber1: 2,
+    // rauchtopaz: 3,
+    // rubin1: 2,
+    // silver1: 2,
+    // zoloto2: 2,
+//   };
+//   const [ingredients, setIngredients] = useState({});
+//   const [price, setPrice] = useState(0);
+//   const [ordering, setOrdering] = useState(false);
+
+//   useEffect(
+//     () => axios
+//       .get('https://builder2-97915-default-rtdb.firebaseio.com/default.json')
+//       .then(response => {
+//         setPrice(response.data.price);
+
+//         // For arrays
+//         // setIngredients(Object.values(response.data.ingredients));
+//         // For objects
+//         setIngredients(response.data.ingredients);
+//       }), []
+//   );
+
+//   function addIngredient(type) {
+//     const newIngredients = { ...ingredients };
+//     newIngredients[type]++;
+//     setPrice(price + prices[type]);
+//     setIngredients(newIngredients);
+//   }
+
+//   function removeIngredient(type) {
+//     if (ingredients[type]) {
+//       const newIngredients = { ...ingredients };
+//       newIngredients[type]--;
+//       setPrice(price - prices[type]);
+//       setIngredients(newIngredients);
+//     }
+//   }
+
+//   function startOrdering() {
+//     setOrdering(true);
+//   }
+
+//   function stopOrdering() {
+//     setOrdering(false);
+//   }
+
+//   return (
+//     <div className={classes.FixingBuilder}>
+//       <FixingPreview
+//         ingredients={ingredients}
+//         price={price} />
+//       <FixingControls
+//         ingredients={ingredients}
+//         addIngredient={addIngredient}
+//         removeIngredient={removeIngredient}
+//         startOrdering={startOrdering}
+//       />
+//       <Modal
+//         show={ordering}
+//         cancel={stopOrdering}>
+//         almaz
+//         zoloto
+//         serebro
+//         </Modal>
+//     </div>
+//   );
+// }
+
+// export default FixingBuilder;
+
+
+
 import FixingPreview from "./FixingPreview/FixingPreview";
 import FixingControls from "./FixingControls/FixingControls";
+
 import classes from "./FixingBuilder.module.css";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Modal from "../UI/Modal/Modal";
+import OrderSummary from "./OrderSummary/OrderSummary";
+import Button from "../UI/Button/Button";
 
-const FixingBuilder = () => {
+const FixingBuilder = ({ history }) => {
   const prices = {
     almaz1: 3,
     amber1: 2,
-    rauchtopaz: 3,
+    rauchtopaz1: 3,
     rubin1: 2,
     silver1: 2,
     zoloto2: 2,
@@ -19,8 +105,10 @@ const FixingBuilder = () => {
   const [price, setPrice] = useState(0);
   const [ordering, setOrdering] = useState(false);
 
-  useEffect(
-    () => axios
+  useEffect(loadDefaults, []);
+
+  function loadDefaults() {
+    axios
       .get('https://builder2-97915-default-rtdb.firebaseio.com/default.json')
       .then(response => {
         setPrice(response.data.price);
@@ -29,8 +117,8 @@ const FixingBuilder = () => {
         // setIngredients(Object.values(response.data.ingredients));
         // For objects
         setIngredients(response.data.ingredients);
-      }), []
-  );
+      });
+  }
 
   function addIngredient(type) {
     const newIngredients = { ...ingredients };
@@ -56,6 +144,22 @@ const FixingBuilder = () => {
     setOrdering(false);
   }
 
+  function finishOrdering() {
+    axios
+      .post('https://builder2-97915-default-rtdb.firebaseio.com/default.json', {
+        ingredients: ingredients,
+        price: price,
+        address: "1234 Jusaeva str",
+        phone: "0 777 777 777",
+        name: "Sadyr Japarov",
+      })
+      .then(() => {
+        setOrdering(false);
+        loadDefaults();
+        history.push('/checkout');
+      });
+  }
+
   return (
     <div className={classes.FixingBuilder}>
       <FixingPreview
@@ -66,18 +170,19 @@ const FixingBuilder = () => {
         addIngredient={addIngredient}
         removeIngredient={removeIngredient}
         startOrdering={startOrdering}
-      />
+        />
       <Modal
         show={ordering}
         cancel={stopOrdering}>
-        almaz
-        zoloto
-        serebro
+          <OrderSummary
+            ingredients={ingredients}
+            price={price}
+            />
+          <Button onClick={finishOrdering} green>Checkout</Button>
+          <Button onClick={stopOrdering}>Cancel</Button>
         </Modal>
     </div>
   );
 }
 
 export default FixingBuilder;
-
-
