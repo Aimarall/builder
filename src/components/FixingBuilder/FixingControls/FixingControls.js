40d77edb3dabd32ad81d3613e071f9b0265d1967
Aimarall <aimaral21072004@gmail.com>
2021-04-29 15:27:42 +0600

    added 1 file to file

--- src/components/FixingBuilder/FixingControls/FixingControls.js
+++ src/components/FixingBuilder/FixingControls/FixingControls.js
@@ -1,4 +1,49 @@
 
+// import Button from "../../UI/Button/Button";
+// import FixingControl from "./FixingControl/FixingControl";
+// import classes from "./FixingControls.module.css";
+
+
+
+
+// const FixingControls = ({
+//   ingredients,
+//   addIngredient,
+//   removeIngredient,
+//   startOrdering
+// })
+
+
+// => 
+
+// {
+//   const results = [];
+//   let total = 0;
+//   for (const ingredient in ingredients) {
+//     // Add ingredient number to totals number
+//     total += ingredients[ingredient];
+//     // Render pizza control for this ingredient
+//     results.push(<FixingControl
+//         key={ingredient}
+//         add={addIngredient}
+//         remove={removeIngredient}
+//         count={ingredients[ingredient]}
+//         type={ingredient} />)
+//   }
+
+//   return (
+//     <div className={classes.FixingControls}>
+//       <strong>Gems</strong>
+//       {results}
+//       <Button disabled={!total} onClick={startOrdering}>Order</Button>
+//     </div>
+//   );
+// }
+
+// export default FixingControls;
+
+
+
 import Button from "../../UI/Button/Button";
 import FixingControl from "./FixingControl/FixingControl";
 import classes from "./FixingControls.module.css";
@@ -8,24 +53,26 @@ const FixingControls = ({
   addIngredient,
   removeIngredient,
   startOrdering
-}) => {
+}) =>
+
+{
   const results = [];
   let total = 0;
   for (const ingredient in ingredients) {
-    // Add ingredient number to totals number
+   
     total += ingredients[ingredient];
-    // Render pizza control for this ingredient
+    
     results.push(<FixingControl
-        key={ingredient}
-        add={addIngredient}
-        remove={removeIngredient}
-        count={ingredients[ingredient]}
-        type={ingredient} />)
+      key={ingredient}
+      add={addIngredient}
+      remove={removeIngredient}
+      count={ingredients[ingredient]}
+      type={ingredient} />)
   }
 
   return (
     <div className={classes.FixingControls}>
-      <strong>Gems</strong>
+      <strong>Ingredients</strong>
       {results}
       <Button disabled={!total} onClick={startOrdering}>Order</Button>
     </div>
