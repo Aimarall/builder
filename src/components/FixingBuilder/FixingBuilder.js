603ba10c9a415969bcd0a4a24fd0ce09e9c34009
Aimarall <aimaral21072004@gmail.com>
2021-04-30 10:17:51 +0600

    added functions

--- src/components/FixingBuilder/FixingBuilder.js
+++ src/components/FixingBuilder/FixingBuilder.js
@@ -1,87 +1,3 @@
-
-// import FixingPreview from "./FixingPreview/FixingPreview";
-// import FixingControls from "./FixingControls/FixingControls";
-// import classes from "./FixingBuilder.module.css";
-// import { useEffect, useState } from "react";
-// import axios from "axios";
-// import Modal from "../UI/Modal/Modal";
-
-// const FixingBuilder = () => {
-//   const prices = {
-    // almaz1: 3,
-    // amber1: 2,
-    // rauchtopaz: 3,
-    // rubin1: 2,
-    // silver1: 2,
-    // zoloto2: 2,
-//   };
-//   const [ingredients, setIngredients] = useState({});
-//   const [price, setPrice] = useState(0);
-//   const [ordering, setOrdering] = useState(false);
-
-//   useEffect(
-//     () => axios
-//       .get('https://builder2-97915-default-rtdb.firebaseio.com/default.json')
-//       .then(response => {
-//         setPrice(response.data.price);
-
-//         // For arrays
-//         // setIngredients(Object.values(response.data.ingredients));
-//         // For objects
-//         setIngredients(response.data.ingredients);
-//       }), []
-//   );
-
-//   function addIngredient(type) {
-//     const newIngredients = { ...ingredients };
-//     newIngredients[type]++;
-//     setPrice(price + prices[type]);
-//     setIngredients(newIngredients);
-//   }
-
-//   function removeIngredient(type) {
-//     if (ingredients[type]) {
-//       const newIngredients = { ...ingredients };
-//       newIngredients[type]--;
-//       setPrice(price - prices[type]);
-//       setIngredients(newIngredients);
-//     }
-//   }
-
-//   function startOrdering() {
-//     setOrdering(true);
-//   }
-
-//   function stopOrdering() {
-//     setOrdering(false);
-//   }
-
-//   return (
-//     <div className={classes.FixingBuilder}>
-//       <FixingPreview
-//         ingredients={ingredients}
-//         price={price} />
-//       <FixingControls
-//         ingredients={ingredients}
-//         addIngredient={addIngredient}
-//         removeIngredient={removeIngredient}
-//         startOrdering={startOrdering}
-//       />
-//       <Modal
-//         show={ordering}
-//         cancel={stopOrdering}>
-//         almaz
-//         zoloto
-//         serebro
-//         </Modal>
-//     </div>
-//   );
-// }
-
-// export default FixingBuilder;
-
-
-
 import FixingPreview from "./FixingPreview/FixingPreview";
 import FixingControls from "./FixingControls/FixingControls";
 
@@ -186,3 +102,5 @@ const FixingBuilder = ({ history }) => {
 }
 
 export default FixingBuilder;
+
+
