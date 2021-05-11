1392c3bcf0bbaf2521f7df7b0844849bc861c17b
Aimarall <aimaral21072004@gmail.com>
2021-05-12 02:13:34 +0600

    deleted noneed elements

--- src/index.js
+++ src/index.js
@@ -5,16 +5,21 @@
 // import { createStore } from 'redux';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
+// import builderReducer from './store/builderReducer';
 
+// const store = createStore(builderReducer);
 
 // ReactDOM.render(
 //   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </Provider>
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+
 
 // // If you want to start measuring performance in your app, pass a function
 // // to log results (for example: reportWebVitals(console.log))
@@ -23,16 +28,25 @@
 
 
 
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import {
+  createStore,
+  applyMiddleware,
+  combineReducers
+} from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import builderReducer from './store/builderReducer';
+import builder from './store/reducers/builder';
+import orders from './store/reducers/orders';
 
-const store = createStore(builderReducer);
+const rootReducer = combineReducers({ builder, orders });
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
@@ -45,9 +59,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
