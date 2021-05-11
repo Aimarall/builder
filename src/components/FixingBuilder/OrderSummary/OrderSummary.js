1777efb70c871cf7ec45c096d5d03fadec556929
Aimarall <aimaral21072004@gmail.com>
2021-05-12 02:14:09 +0600

    changed functions of the files

--- src/components/FixingBuilder/OrderSummary/OrderSummary.js
+++ src/components/FixingBuilder/OrderSummary/OrderSummary.js
@@ -1,4 +1,41 @@
 
+// import classes from "./OrderSummary.module.css";
+
+// const OrderSummary = ({ ingredients, price }) => {
+//   const labels = {
+    // diamond: "Diamond",
+    // amber: "Amber",
+    // rauchtopaz: "Rauchtopaz",
+    // ruby: "Ruby",
+    // silver: "Silver",
+    // gold: "Gold",
+//   }
+//   const results = Object.keys(ingredients)
+//     .map(type => <li>{labels[type]}: {ingredients[type]}</li>);
+
+//   return (
+//     <div className={classes.OrderSummary}>
+//       <h3>Order summary</h3>
+//       <ul>
+//         {results}
+//       </ul>
+//       <strong>Total price: {price.toFixed(1)} som</strong>
+//     </div>
+//   );
+// }
+
+// export default OrderSummary;
+
+
+
+
+
+
+
+
+
+
+
 import classes from "./OrderSummary.module.css";
 
 const OrderSummary = ({ ingredients, price }) => {
@@ -11,7 +48,7 @@ const OrderSummary = ({ ingredients, price }) => {
     gold: "Gold",
   }
   const results = Object.keys(ingredients)
-    .map(type => <li>{labels[type]}: {ingredients[type]}</li>);
+    .map(type => <li key={type}>{labels[type]}: {ingredients[type]}</li>);
 
   return (
     <div className={classes.OrderSummary}>
