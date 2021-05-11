fdd05094c0722c9d30da08f4c9a7b6a81b482cc2
Aimarall <aimaral21072004@gmail.com>
2021-05-12 02:14:43 +0600

    changed functions of the files

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,26 +1,51 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+// import Order from "./Order/Order";
+// import classes from "./Orders.module.css";
+
+// const Orders = () => {
+//     const [orders, setOrders] = useState([]);
+
+//     useEffect(() => {
+//         axios.get('https://builder2-97915-default-rtdb.firebaseio.com/orders.json')
+//             .then(response => {
+//                 setOrders(Object.values(response.data));
+//             });
+//     }, []);
+
+//     const results = orders.map(order => <Order {...order} />);
+
+//     return (
+//         <div className={classes.Orders}>
+//             {results}
+//         </div>
+//     );
+// }
+
+// export default Orders;
+
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { load } from "../../store/actions/orders";
 import Order from "./Order/Order";
+import withAxios from "../withAxios";
+import axios from "../../axios";
 import classes from "./Orders.module.css";
 
 const Orders = () => {
-    const [orders, setOrders] = useState([]);
+  const dispatch = useDispatch();
+  const orders = useSelector(state => state.orders);
 
-    useEffect(() => {
-        axios.get('https://builder2-97915-default-rtdb.firebaseio.com/orders.json')
-            .then(response => {
-                setOrders(Object.values(response.data));
-            });
-    }, []);
+  useEffect(() => dispatch(load()), [dispatch]);
 
-    const results = orders.map(order => <Order {...order} />);
+  const results = orders.map(order => <Order key={order.id} {...order} />);
 
-    return (
-        <div className={classes.Orders}>
-            {results}
-        </div>
-    );
+  return (
+    <div className={classes.Orders}>
+      {results}
+    </div>
+  );
 }
 
-export default Orders;
-
+export default withAxios(Orders, axios);
