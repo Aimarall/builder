e745474b7735be3cb5a9e148ef89f213537e4908
Aimarall <aimaral21072004@gmail.com>
2021-05-12 02:15:06 +0600

    added axios https to file

--- src/store/builderReducer.js
+++ src/store/builderReducer.js
@@ -1,42 +1,42 @@
-const initialState = {
-    ingredients: {
-        diamond: 5,
-        amber: 5,
-        rauchtopaz: 10,
-        ruby: 2,
-        silver: 12,
-        gold: 2,
-    },
-    price: 100,
-};
-const prices = {
-    diamond: 3,
-    amber: 2,
-    rauchtopaz: 3,
-    ruby: 2,
-    silver: 2,
-    gold: 2,
-};
+// const initialState = {
+//     ingredients: {
+//         diamond: 5,
+//         amber: 5,
+//         rauchtopaz: 10,
+//         ruby: 2,
+//         silver: 12,
+//         gold: 2,
+//     },
+//     price: 100,
+// };
+// const prices = {
+//     diamond: 3,
+//     amber: 2,
+//     rauchtopaz: 3,
+//     ruby: 2,
+//     silver: 2,
+//     gold: 2,
+// };
 
-const builderReducer = (state = initialState, action) => {
-    const newState = { ...state };
+// const builderReducer = (state = initialState, action) => {
+//     const newState = { ...state };
 
-    switch (action.type) {
-        case "ADD_INGREDIENT":
-            newState.ingredients[action.ingredient]++;
-            newState.price += prices[action.ingredient];
-            break;
-        case "REMOVE_INGREDIENT":
-            newState.ingredients[action.ingredient]--;
-            newState.price -= prices[action.ingredient];
+//     switch (action.type) {
+//         case "ADD_INGREDIENT":
+//             newState.ingredients[action.ingredient]++;
+//             newState.price += prices[action.ingredient];
+//             break;
+//         case "REMOVE_INGREDIENT":
+//             newState.ingredients[action.ingredient]--;
+//             newState.price -= prices[action.ingredient];
 
-            break;
+//             break;
 
-        default:
-            break;
-    }
+//         default:
+//             break;
+//     }
 
-    return newState;
-}
+//     return newState;
+// }
 
-export default builderReducer;
+// export default builderReducer;
